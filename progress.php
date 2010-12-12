a38709bd33344f73758f5e3b6d15531a13c31255
septor <patrickweaver@gmail.com>
2010-12-11 22:54:55 -0500

    layout phase complete

--- progress.php
+++ progress.php
@@ -7,27 +7,38 @@ if(!defined("e107_INIT")) {
 if(!getperms("P")){ header("location:".e_BASE."index.php"); exit;}
 require_once(e_ADMIN."auth.php");
 
-$text = "<div style='text-align:center'>
-<table style='width:75%' class='fborder'>
-<tr>
-<td class='fcaption'>Boss Name</td>
-<td class='fcaption'>Instance</td>
-<td class='fcaption'>Normal Status</td>
-<td class='fcaption'>Heroic Status</td>
-</tr>";
+$text = "<div style='text-align:center'>";
 
-$sql->db_Select("wowprogress_bosses", "*") or die(mysql_error());
+$sql->db_Select("wowprogress_instances", "*") or die(mysql_error());
 
 while($row = $sql->db_Fetch()){
-	$text .= "<tr>
-	<td class='forumheader3'><a href='http://www.wowhead.com/".$row['npctype']."=".$row['npcid']."'>".$row['bossname']."</a></td>
-	<td class='forumheader3'>".$row['instance']."</td>
-	<td class='forumheader3'>".$row['status']."</td>
-	<td class='forumheader3'>".$row['heroic']."</td>
-	</td>";
+
+	$text .= "<table style='width:75%' class='fborder'>
+	<tr>
+	<td colspan='".($row['heroic'] == "1" ? "3" : "2")."' class='fcaption' style='text-align:center;'><b><u>".$row['zonename']."</u></b></td>
+	</tr>
+	<tr>
+	<td class='forumheader3' style='width:70%'><b>Boss Name</b></td>
+	<td class='forumheader3'><b>Normal Status</b></td>
+	".($row['heroic'] == "1" ? "<td class='forumheader3'><b>Heroic Status</b></td>" : "")."
+	</tr>";
+
+	$sql2->db_Select("wowprogress_bosses", "*", "instance='".addslashes($row['zonename'])."'") or die(mysql_error());
+
+	while($row2 = $sql2->db_Fetch()){
+		$text .= "<tr>
+		<td class='forumheader3'><a href='http://www.wowhead.com/".$row2['npctype']."=".$row2['npcid']."'>".$row2['bossname']."</a></td>
+		<td class='forumheader3'>".$row2['status']."</td>
+		".($row['heroic'] == "1" ? "<td class='forumheader3'>".$row2['heroic']."</td>" : "")."
+		</tr>";
+	}
+
+	$text .= "</table>
+	<br />";
+
 }
 
-$text .= "</table>";
+$text .= "</div>";
 
 $ns->tablerender("Manage Progression", $text);
 require_once(e_ADMIN."footer.php");
