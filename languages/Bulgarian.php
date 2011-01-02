b533bd550157a0431b44d10ca51247f88314bd20
septor <patrickweaver@gmail.com>
2011-01-02 10:30:53 -0500

    Bulgarian translations

---
+++ languages/Bulgarian.php
@@ -0,0 +1,62 @@
+<?php
+
+// compatible with 0.1 and under
+
+
+// admin_config.php
+define("WPCONFIG_LAN001", "Настройките са записани успешно!");
+define("WPCONFIG_LAN002", "Кой може да управлява убийствата на шефа?");
+define("WPCONFIG_LAN003", "Кои случай искаш да се показват в менюто?");
+define("WPCONFIG_LAN004", "Нямате нито един случай в базата данни!");
+define("WPCONFIG_LAN005", "Добавете няколко");
+define("WPCONFIG_LAN006", "Кои стил за убиването на прогресия искате да се показва от заглавие на случай?");
+define("WPCONFIG_LAN007", "Общо");
+define("WPCONFIG_LAN008", "Нормален");
+define("WPCONFIG_LAN009", "Героичен");
+define("WPCONFIG_LAN010", "Няма");
+define("WPCONFIG_LAN011", "Стил, използван за заглавията на случаите");
+define("WPCONFIG_LAN012", "Запиши настройките");
+define("WPCONFIG_LAN013", "Конфигурирай WoW Progression Меню");
+
+
+// datapack.php
+define("WPDPACK_LAN001", "Вие успешно добавихте ");
+define("WPDPACK_LAN002", " случай(и) и ");
+define("WPDPACK_LAN003", " шеф(ове) във Вашата база данни.");
+define("WPDPACK_LAN004", "Щракнете тук, за да управлявате Вашите нови данни.");
+define("WPDPACK_LAN005", "WoW Прогресия Пакет на данните");
+define("WPDPACK_LAN006", "WoW Прогресия Пакет Не е намерен!");
+define("WPDPACK_LAN007", "Изглежда, че нямате файлът dataz.xml на разположение");
+define("WPDPACK_LAN008", "Моля, вземете най-новото копие и изпълнете отново този скрипт!");
+
+// manage.php
+define("WPMANAGE_LAN001", "Успешно е обновен статусът на прогресията!");
+define("WPMANAGE_LAN002", "Име на шефа");
+define("WPMANAGE_LAN003", "Статус Нормален");
+define("WPMANAGE_LAN004", "Статус Героичен");
+define("WPMANAGE_LAN005", "Не е убиван");
+define("WPMANAGE_LAN006", "Опит");
+define("WPMANAGE_LAN007", "Убити");
+define("WPMANAGE_LAN008", "Обнови ");
+define("WPMANAGE_LAN009", "Изчисти");
+define("WPMANAGE_LAN010", "Управлявай прогресията");
+define("WPMANAGE_LAN011", "Грешка!");
+define("WPMANAGE_LAN012", "Не може да се управлявате прогресията. Свържете се с Вашия Шеф на Гилдията!");
+
+// plugin.php
+define("WPPLUGIN_LAN001", "World of Warcraft Шеф Прогресия Меню");
+define("WPPLUGIN_LAN002", "Конфигурирай WoW прогресия Меню");
+define("WPPLUGIN_LAN003", " е успешно инсталирано! Ако искате да инсталирате шефове и пакет случаи, ");
+define("WPPLUGIN_LAN004", "моля щракнете тук");
+
+// wowprogress_menu.php
+
+define("WPMENU_LAN001", "Изчистихме следното:");
+define("WPMENU_LAN002", "Нормален Режим");
+define("WPMENU_LAN003", "Героичен Режим");
+define("WPMENU_LAN004", "Не е Убиван");
+define("WPMENU_LAN005", "Опит");
+define("WPMENU_LAN006", "Убити");
+define("WPMENU_LAN007", "прогрес на Нападение");
+
+?>
